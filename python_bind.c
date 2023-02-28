78c7351c1a004d728f74e50a88c434d2a000b6e2
georkapetanos <hellenicglavkosmos@gmail.com>
2023-02-28 23:41:14 +0200

    string parsing

--- python_bind.c
+++ python_bind.c
@@ -56,9 +56,56 @@ static PyObject *method_transmit_encrypted(PyObject *self, PyObject *args) {
 	return PyLong_FromLong(0);
 }
 
+static PyObject *method_process_detections_str(PyObject *self, PyObject *args) {
+	char *token, *old_str, *new_str, returned_str[512], value[4], new_value[4], *occur;
+	int iterations, old_iterations;
+
+	if(!PyArg_ParseTuple(args, "ss", &old_str, &new_str)) {
+		return NULL;
+	}
+	
+	strcpy(returned_str, old_str);
+	
+	token = strtok(new_str, ",");
+	if(!strncmp(token, "(no detections)", 15)) {
+		//no detections do nothing
+		return PyUnicode_FromString(old_str);
+	}
+	while (token != NULL) {
+		if(!strncmp(token, " ", 1)) {
+			//workaround first character space
+			token++;
+		}
+		iterations = atoi(token);
+		//object string is at strstr(token, " ") + 1
+		printf("iter: %d, string: %s.\n", iterations, strstr(token, " ") + 1);
+		occur = strstr(returned_str, strstr(token, " ") + 1);
+		if(occur != NULL) {
+			old_iterations = atoi(occur - 4);
+			printf("old_iterations = %d\n", old_iterations);
+			sprintf(new_value, "%3d", old_iterations + iterations);
+			*(occur - 4) = new_value[0];
+			*(occur - 3) = new_value[1];
+			*(occur - 2) = new_value[2];
+		}
+		else {
+			if(strlen(returned_str) != 0) {
+				strcat(returned_str, ", ");
+			}
+			sprintf(value, "%3d", iterations);
+			strcat(returned_str, value);
+			strcat(returned_str, " ");
+			strcat(returned_str, strstr(token, " ") + 1);
+		}
+		token = strtok(NULL, ",");
+	}
+	return PyUnicode_FromString(returned_str);
+}
+
 static PyMethodDef LrdMethods[] = {
 	{"transmit", method_transmit, METH_VARARGS, "Transmit string message"},
 	{"transmit_encrypted", method_transmit_encrypted, METH_VARARGS, "Encrypt and transmit string message"},
+	{"process_detections_str", method_process_detections_str, METH_VARARGS, "Process detections str"},
 	{NULL, NULL, 0, NULL}
 };
 
