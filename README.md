736e19e8b5f5bdd7e43b09dbe48b6e47dedc97d0
georkapetanos <hellenicglavkosmos@gmail.com>
2023-03-01 23:48:31 +0200

    Readme.md, camera python script

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
-##LoRa Peer to Peer link for message and command delivering
+### LoRa network layer for Peer to Peer messaging and command delivering
 
-### Requirements: libmosquitto-dev, uuid-dev, libssl-dev
+#### Requirements: 
+* libmosquitto-dev
+* uuid-dev
+* libssl-dev
 
-### Examples:
-Send JSON message using serial0 interface
-./lrd -t /dev/serial0 -s "Test message"
-Encrypt and send JSON message
-./lrd -t /dev/serial0 -e -s "Test message"
-Receive JSON message using ttyUSB0 interface
+#### Features
+
+* Receiver and Transmitter functionality
+* UART LoRa interface
+* Error detection with embedded checksum
+* OpenSSL EVP based encryption
+* Message acknowledgement
+* MQTT message forwarding
+
+#### System Diagram
+
+<img src="Documentation/diagram.png" width="600" />
+
+#### Raspberry Pi 4 Model B + Sony IMX708 (Rpi Camera Module 3) + Ebyte E32-868T20D
+
+<img src="Documentation/rpi_hardware_configuration.jpg" width="400" />
+
+#### Ebyte E32-868T30D <--> Raspberry Pi Pinout
+
+<img src="Documentation/e32-868_pinout.png" width="400" />
+
+#### Examples:
+
+Send JSON message using serial0 interface:
+```
+./lrd -t /dev/serial0 -s "message"
+```
+Encrypt and send JSON message:
+```
+./lrd -t /dev/serial0 -e -s "message"
+```
+
+Receive JSON message using ttyUSB0 interface:
+```
 ./lrd -t /dev/ttyUSB0 -r
-Receive encrypted JSON message
+```
+
+Receive encrypted JSON message:
+```
 ./lrd -t /dev/ttyUSB0 -e -r
-Receive broadcasted messages from MQTT
+```
+
+Receive broadcasted messages from MQTT:
+```
 mosquitto_sub -h hostname -u username -P password -t "lrdlink"
+```
---
+++ camera_detect.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python3
+from picamera2 import Picamera2
+from functools import partial
+import sys, time, libcamera, signal, torch, re
+import lrd
+
+CAPTURED_IMG_PATH = "/media/ramdisk/picam.jpg"
+TRANSMIT_INTERVAL = 40
+
+def camera_setup():
+	picam = Picamera2()
+	config = picam.create_preview_configuration(main={"size": (3840, 2160)})
+	config["transform"] = libcamera.Transform(hflip=1, vflip=1)
+	picam.configure(config)
+	picam.start()
+	return picam
+
+def model_setup():
+	model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
+	return model
+
+def capture_and_process(picam, model):
+	picam.capture_file(CAPTURED_IMG_PATH)
+	results = model(CAPTURED_IMG_PATH)
+	output = str(results.print)
+	#print(output)
+	output = output.split("\n",5)[1]
+	output = output.split(" ", 3)[3]
+	print(output)
+	#results.save()
+	return output
+
+def sigint_handler(sig, frame, picam):
+	print('\nQuitting...')
+	picam.close()
+	sys.exit(0)
+
+def main():
+	global picam, output, model, handler, detections
+	picam = camera_setup()
+	handler = partial(sigint_handler, picam=picam)
+	signal.signal(signal.SIGINT, handler)
+	model = model_setup()
+	detections = ""
+
+	start = time.time()
+
+	while(True):
+		output = capture_and_process(picam, model)
+		#print(output)
+		detections = lrd.process_detections_str(detections, output)
+		#print(detections)
+		print(detections + " " + str(time.time() - start))
+		if (time.time() - start > TRANSMIT_INTERVAL):
+			#transmit detections
+			detections = re.sub(r"\s+", " ", detections)
+			lrd.transmit_encrypted(1, detections, "/dev/serial0")
+			detections = ""
+			start = time.time()
+
+if __name__ == "__main__":
+	main()
