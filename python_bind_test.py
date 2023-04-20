74d7b923f7c7b35779ea24b4fa9ae9586839f2d2
georkapetanos <hellenicglavkosmos@gmail.com>
2023-04-20 15:58:00 +0300

    Python bind test relocation

--- python_bind_test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import lrd
-import time
-lrd.__doc__
-lrd.__name__
-#lrd.transmit(1, "cat,3", "/dev/ttyUSB0")
-#time.sleep(2)
-lrd.transmit_encrypted(1, "boat,2", "/dev/ttyUSB0")
