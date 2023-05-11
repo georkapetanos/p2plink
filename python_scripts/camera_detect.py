fde1f547635908cdd6813be9be8d992b73a52d5a
georkapetanos <hellenicglavkosmos@gmail.com>
2023-05-12 00:18:33 +0300

    Added multithreading to camera detect python script

--- python_scripts/camera_detect.py
+++ python_scripts/camera_detect.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 from picamera2 import Picamera2
 from functools import partial
-import sys, time, libcamera, signal, torch, re
+import sys, time, libcamera, signal, torch, re, threading
 import lrd
 
-CAPTURED_IMG_PATH = "/media/ramdisk/picam.jpg"
-TRANSMIT_INTERVAL = 60
+CAPTURED_IMG_PATH = "/media/ramdisk/picam0.jpg"
+CAPTURED_IMG_PATH_ALT = "/media/ramdisk/picam1.jpg"
+TRANSMIT_INTERVAL = 30
 
 def camera_setup():
 	picam = Picamera2()
-	config = picam.create_preview_configuration(main={"size": (3840, 2160)})
+	config = picam.create_preview_configuration(main={"size": (3840, 1920)})
 	config["transform"] = libcamera.Transform(hflip=1, vflip=1)
 	picam.configure(config)
 	picam.start()
@@ -19,15 +20,15 @@ def model_setup():
 	model = torch.hub.load('ultralytics/yolov5', 'yolov5n')
 	return model
 
-def capture_and_process(picam, model):
-	picam.capture_file(CAPTURED_IMG_PATH)
-	results = model(CAPTURED_IMG_PATH)
+def capture_image(picam, path):
+	picam.capture_file(path)
+
+def process_image(model, path):
+	results = model(path)
 	output = str(results.print)
-	#print(output)
 	output = output.split("\n",5)[1]
 	output = output.split(" ", 3)[3]
 	print(output)
-	#results.save()
 	return output
 
 def sigint_handler(sig, frame, picam):
@@ -37,7 +38,10 @@ def sigint_handler(sig, frame, picam):
 
 def main():
 	global picam, output, model, handler, detections
+	select_image = True #This toggles image for multithreading, False -> save to ALT_image
 	picam = camera_setup()
+	#capture first image
+	capture_image(picam, CAPTURED_IMG_PATH_ALT)
 	handler = partial(sigint_handler, picam=picam)
 	signal.signal(signal.SIGINT, handler)
 	model = model_setup()
@@ -47,11 +51,26 @@ def main():
 	processings = 0
 
 	while(True):
-		output = capture_and_process(picam, model)
+		#dt_cam = time.time()
+		if select_image:
+			cam_thread = threading.Thread(target=capture_image, args=(picam, CAPTURED_IMG_PATH,))
+		else:
+			cam_thread = threading.Thread(target=capture_image, args=(picam, CAPTURED_IMG_PATH_ALT,))
+		cam_thread.start()
+		#dt_cam = time.time() - dt_cam
 		#print(output)
+		dt_detect = time.time()
+		if select_image:
+			output = process_image(model, CAPTURED_IMG_PATH_ALT)
+		else:
+			output = process_image(model, CAPTURED_IMG_PATH)
+		select_image = not select_image
+			
 		detections = lrd.process_detections_str(detections, output)
+		dt_detect = time.time() - dt_detect
 		processings = processings + 1
 		#print(detections)
+		print("dt_detect = "+"{:.2f}".format(dt_detect))
 		print("dt = " + "{:.2f}".format(time.time() - start) + ", p = " + str(processings) + " || " + detections)
 		if (time.time() - start > TRANSMIT_INTERVAL):
 			#transmit detections
